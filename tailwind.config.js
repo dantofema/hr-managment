0ae9ff54c426ff753bb642cbd7ff7b78fbc1b160
alejandro <dantofema@gmail.com>
2025-08-06 00:53:18 -0300

    add Tailwind CSS configuration for styling components and templates

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+    content: [
+        './js/**/*.{vue,js,ts,jsx,tsx}',
+        './components/**/*.{vue,js,ts,jsx,tsx}',
+        './App.vue',
+        './main.js',
+        '../templates/**/*.twig',
+    ],
+    theme: {
+        extend: {},
+    },
+    plugins: [],
+};
